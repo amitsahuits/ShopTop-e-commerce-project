09bcf1f2af9f171c5fb97d429ab56f65d284f255
Amit sahu <amitsahuits@gmail.com>
2021-10-18 21:13:31 +0530

    changed requirements.txt

--- Shoptop/settings.py
+++ Shoptop/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/productimg/'
+MEDIA_ROOT = BASE_DIR / '/app/media/productimg/'
 LOGIN_REDIRECT_URL = '/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
